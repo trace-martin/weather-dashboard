cf844550404819e778969f593379eae099f7421e
trace-martin <trace.martin7817@gmail.com>
2023-04-03 23:00:12 -0600

    updated README/added search container w/ input/started var declaration js

--- assets/script.js
+++ assets/script.js
@@ -1,1 +1,12 @@
+// CONST API key
 const apiKey = '9d98a835a05f7570c57fc8265a0dbc16'
+// Declare var for search history
+var searchHistory = [];
+// Declare other var
+var searchForm = $('#searchForm');
+var searchButton = $('#searchButton');
+var clearButton = $('#clearButton');
+var currentWeather = $('#currentWeather');
+var currentTemp = $('#currentTemperature');
+var currentHumidty = $('#humidity')
+
