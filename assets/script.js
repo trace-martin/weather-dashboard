824b596836b281b1ba48d564c7b41729e89ca51a
trace-martin <trace.martin7817@gmail.com>
2023-04-03 23:46:34 -0600

    added current weather function but does not work need to add classes and fix link

--- assets/script.js
+++ assets/script.js
@@ -10,3 +10,22 @@ var currentWeather = $('#currentWeather');
 var currentTemp = $('#currentTemperature');
 var currentHumidty = $('#humidity')
 
+
+searchButton.addEventListener('click', getCurrentWeather());
+
+function getCurrentWeather(){
+
+    var queryURL = 'https://api.openweathermap.org/data/2.5/weather?lat={lat}&lon={lon}&appid={apiKey
+    console.log("queryURL");
+    fetch(queryURL)
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        console.log(data);
+        catPic = data[0];
+        console.log("Here is the cat picture: " + catPic);
+      });
+      // delay is here to ensure the call to the api is complete otherwise we might set the attribute while data is empty
+    await delay(500); 
+  }
